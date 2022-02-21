90d1ff50364b80e50c233387224340171602b725
Developer Usharani <99324598+Developer-usharani@users.noreply.github.com>
2022-02-21 16:20:15 +0530

    Add files via upload

---
+++ sql query.sql
@@ -0,0 +1,8 @@
+create database database1;
+use database1;
+create table student(
+s_name varchar(20),s_id int,s_branch varchar(18),s_subject varchar(20)
+);
+insert into student values("usharani",13,"bccs","computer science");
+insert into student values("sowmya",8,"bccs","chemistry");
+insert into student values("rajitha",20,"bgc","genetics");
